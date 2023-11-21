efb05ba26780e64bdb894bae8d884550bac65121
aalexebr <alex.ebr@icloud.com>
2023-11-21 14:32:04 +0100

    refactor2

--- src/org/java/pojo/abs/Event.java
+++ src/org/java/pojo/abs/Event.java
@@ -36,7 +36,7 @@ public class Event {
 		LocalDate today = LocalDate.now();
 		LocalDate expectedDate = LocalDate.of(year, month, day);
 		if(expectedDate.isBefore(today)) {
-			 throw new Exception("Date inserted has passed");
+			 throw new Exception("ERROR : Date inserted is expired");
 		}
 		else {
 			this.date = LocalDate.of(year, month, day);
@@ -66,19 +66,19 @@ public class Event {
 			this.reservedSeats++;
 			return;
 		}
-		throw new Exception("The event date has already passed");
+		throw new Exception("ERROR : The event date has already passed");
 	}
 	
 	public void cancelReservation () throws Exception{
 		LocalDate today = LocalDate.now();
 		if(getReservedSeats()<=0) {
-			throw new Exception("No reservations to cancel");
+			throw new Exception("ERROR : No reservations to cancel");
 		}
 		if(getDate().isAfter(today) || getDate().isEqual(today) && getReservedSeats()>0) {
 			this.reservedSeats--;
 			return;
 		}
-		throw new Exception("The event date has already passed");
+		throw new Exception("ERROR : The event date has already passed");
 	}
 	
 	public void makeMultipleReservations(int n) throws Exception{
@@ -88,40 +88,35 @@ public class Event {
 			throw new Exception("reserving more seats than available");
 		}
 		if(getDate().isAfter(today) || getDate().isEqual(today)) {
-			this.reservedSeats = n;
+			this.reservedSeats += n;
 			return;
 		}
-		throw new Exception("The event date has already passed");
+		throw new Exception("ERROR : The event date has already passed");
 	}
 	
 	public void cancelMultipleReservations(int n ) throws Exception{
 		LocalDate today = LocalDate.now();
 		if(getReservedSeats()<=0) {
-			throw new Exception("No reservations to cancel");
+			throw new Exception("ERROR : No reservations to cancel");
 		}
-		else if(getReservedSeats()<= n) {
-			throw new Exception("number of reserved seats is less than the number you are trying to cancel");
+		else if(getReservedSeats()< n) {
+			throw new Exception("ERROR : number of reserved seats is less than the number you are trying to cancel");
 		}
 		if(getDate().isAfter(today) || getDate().isEqual(today)) {
 			this.reservedSeats -= n;
 			return;
 		}
-		throw new Exception("The event date has already passed");
+		throw new Exception("ERROR : The event date has already passed");
 		
 	}
 
 	public String getFormatDate() {
-		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-//		Date date = new Date();
-//		System.out.println(dateFormat.format(getDate()));
-//		return dateFormat.format(getDate());
+//		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 		
 		String pattern = "dd/MM/yyyy";
 
-        // Create a DateTimeFormatter with the desired pattern
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
 
-        // Format the LocalDate using the formatter
         String formattedDate = getDate().format(formatter);
         return formattedDate;
 	}
@@ -138,8 +133,7 @@ public class Event {
 		return "EVENT \n"+
 				"name: "+ getTitle() + "\n"+
 				"date: "+ getFormatDate() + "\n"+
-				"tot seats "+getTotSeats()+ "\n"+
-				"reserved: "+getReservedSeats() + "\n";
+				getReservationsAndTotalSeats();
 				
 		
 	}
