f9a89c41b3360fad268bb63055e0665cc7864429
aalexebr <alex.ebr@icloud.com>
2023-11-21 14:09:56 +0100

    bonus

--- src/org/java/Main.java
+++ src/org/java/Main.java
@@ -3,22 +3,109 @@ package org.java;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.Scanner;
 
 import org.java.pojo.abs.Concert;
 import org.java.pojo.abs.Event;
+import org.java.pojo.abs.EventProgram;
 
 public class Main {
 	public static void main(String[] args) {
+
+//		String name;
+//		int day;
+//		int month;
+//		int year;
+//		int totSeats;
+//		Scanner in = new Scanner(System.in);
+//		System.out.println("create event ");
+//		
+//		System.out.print("name of event:");
+//		name = in.nextLine();
+//		
+//		System.out.print("day of event:");
+//		String userDay = in.nextLine();
+//		day = Integer.valueOf(userDay);
+//		
+//		System.out.print("month of event as number:");
+//		String userMonth = in.nextLine();
+//		month = Integer.valueOf(userMonth);
+//		
+//		System.out.print("year of event as number:");
+//		String userYear = in.nextLine();
+//		year = Integer.valueOf(userYear);
+//		
+//		System.out.print("event capacity:");
+//		String userCapacity = in.nextLine();
+//		totSeats = Integer.valueOf(userCapacity);
+//		
+//		Event event = null;
+//		
+//		try {
+//			
+//			 event = new Event(name, year, month, day, totSeats);
+//			System.out.println(event);
+//			
+//			
+//		} 
+//		catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+//		
+//			char choice;
+//			System.out.print("do you want to make a reservation? y/n");
+//			String userChoise = in.nextLine();
+//			choice = userChoise.charAt(0);
+//			if( choice == 'y') {
+//				int res;
+//				System.out.print("how many seats to reserve?");
+//				String userRez = in.nextLine();
+//				res = Integer.valueOf(userRez);
+//				
+//				try {
+//					event.makeMultipleReservations(res);
+//				} catch (Exception e) {
+//					System.out.println(e.getMessage());
+//				}
+//				
+//				System.out.println(event.getReservationsAndTotalSeats());
+//				
+//				int cancel;
+//				
+//				System.out.print("how many reservations to cancel?");
+//				String userCancel = in.nextLine();
+//				cancel = Integer.valueOf(userCancel);
+//				
+//				try {
+//					event.cancelMultipleReservations(cancel);
+//					
+//				} catch (Exception e) {
+//					System.out.println(e.getMessage());
+//				}
+//				System.out.println(event.getReservationsAndTotalSeats());
+//				
+//			}
+//			else {
+//				System.out.println("terminated");
+//		}
+//			System.out.println(event);
+//		
+//		
+//		
+//		in.close();
+		
+		
+		
 //		CONCERT MILESTONe3
-		try {
-			Concert x = new Concert("event1", 2023,12,12, 1000,"99.12",12,30);
-			System.out.println(x);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			System.out.println(e.getMessage());
-		}
+//		try {
+//			Concert concert1 = new Concert("event1", 2023,12,12, 1000,"99.12",12,30);
+//			System.out.println(concert1);
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			System.out.println(e.getMessage());
+//		}
 //		milestone 1
 //		try {
 //			Event x = new Event("event1", 2023,8,12, 1000);
@@ -49,89 +136,50 @@ public class Main {
 //		}
 //		
 //		System.out.println(x);
-		String name;
-		int day;
-		int month;
-		int year;
-		int totSeats;
-		Scanner in = new Scanner(System.in);
-		System.out.println("create event ");
 		
-		System.out.print("name of event:");
-		name = in.nextLine();
+//		BONUS
 		
-		System.out.print("day of event:");
-		String userDay = in.nextLine();
-		day = Integer.valueOf(userDay);
-		
-		System.out.print("month of event as number:");
-		String userMonth = in.nextLine();
-		month = Integer.valueOf(userMonth);
-		
-		System.out.print("year of event as number:");
-		String userYear = in.nextLine();
-		year = Integer.valueOf(userYear);
-		
-		System.out.print("event capacity:");
-		String userCapacity = in.nextLine();
-		totSeats = Integer.valueOf(userCapacity);
-		
-		Event event = null;
+		Concert concert1 = null;
+		Event event1 = null;
+		Concert concert2 = null;
+		Event event2 = null;
 		
 		try {
-			
-			 event = new Event(name, year, month, day, totSeats);
-			System.out.println(event);
-			
-			
-		} 
-		catch (Exception e) {
+			concert1 = new Concert("concert1", 2023, 12, 23, 101, "59.99", 12, 30);
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 		
-			char choice;
-			System.out.print("do you want to make a reservation? y/n");
-			String userChoise = in.nextLine();
-			choice = userChoise.charAt(0);
-			if( choice == 'y') {
-				int res;
-				System.out.print("how many seats to reserve?");
-				String userRez = in.nextLine();
-				res = Integer.valueOf(userRez);
-				
-				try {
-					event.makeMultipleReservations(res);
-				} catch (Exception e) {
-					System.out.println(e.getMessage());
-				}
-				
-				System.out.println(event.getReservationsAndTotalSeats());
-				
-				int cancel;
-				
-				System.out.print("how many reservations to cancel?");
-				String userCancel = in.nextLine();
-				cancel = Integer.valueOf(userCancel);
-				
-				try {
-					event.cancelMultipleReservations(cancel);
-					
-				} catch (Exception e) {
-					System.out.println(e.getMessage());
-				}
-				System.out.println(event.getReservationsAndTotalSeats());
-				
-			}
-			else {
-				System.out.println("terminated");
+		try {
+			concert2 = new Concert("concert2", 2023, 12, 23, 500, "200.99", 21, 30);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
-			System.out.println(event);
 		
+		try {
+			event1 = new Event("event1", 2023, 12, 24, 101);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		try {
+			event2 = new Event("event2", 2023, 12, 24, 101);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 		
+		EventProgram prog = new EventProgram("Program1");
+		System.out.println(prog.getProgramLength());
 		
-		in.close();
+		prog.addEvent(concert1);
+		System.out.println(prog.getProgramLength());
+		prog.addEvent(concert2);
+		prog.addEvent(event2);
 		
+		System.out.println(prog.eventsInDate(2023,12,23));
 		
+		prog.deleteAllEvents();
+		
+		prog.printList();
 		
 	}
 
---
+++ src/org/java/pojo/abs/EventProgram.java
@@ -0,0 +1,70 @@
+package org.java.pojo.abs;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventProgram {
+	
+	private String title;
+	private List <Event> events;
+	
+	public EventProgram(String title) {
+		setEvents();
+		setTitle(title);
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public List<Event> getEvents() {
+		return events;
+	}
+
+	public void setEvents() {
+		this.events =  new ArrayList <>();;
+	}
+	
+	public void addEvent(Event event) {
+		getEvents().add(event);
+	}
+	
+	public int getProgramLength() {
+		return getEvents().size();
+	}
+	
+	public void deleteAllEvents() {
+		getEvents().clear();
+	}
+	
+	public List eventsInDate(int year, int month, int day) {
+		LocalDate date = LocalDate.of(year, month, day);
+		List <Event> newList = new ArrayList<>();
+
+		 for (int i = 0; i < getProgramLength(); i++) {
+			 Event event = getEvents().get(i);
+
+			 if(event.getDate().isEqual(date)) {
+	            	newList.add(event);
+	            }
+	        }
+        return newList;
+	}
+	
+	public void printList() {
+		System.out.println("Program: " +getTitle());
+		for (int i = 0; i < getProgramLength(); i++) {
+			 Event event = getEvents().get(i);
+			 System.out.println(event);
+	        }
+	}
+	
+
+
+	
+}
